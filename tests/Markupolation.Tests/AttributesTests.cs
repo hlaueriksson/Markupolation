8b396ef631b5c7d3cd0cd2e4efccd2e466dc3e40
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-10-03 01:08:38 +0200

    tests

---
+++ tests/Markupolation.Tests/AttributesTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Markupolation.Tests
+{
+    public class AttributesTests
+    {
+        [Test]
+        public void NormalAttribute()
+        {
+            var result = href("https://html.spec.whatwg.org/multipage/");
+            result.ToString().Should().Be("href=\"https://html.spec.whatwg.org/multipage/\"");
+        }
+
+        [Test]
+        public void BooleanElement()
+        {
+            var result = crossorigin(null);
+            result.ToString().Should().Be("crossorigin");
+
+            result = crossorigin("");
+            result.ToString().Should().Be("crossorigin=\"\"");
+
+            result = required();
+            result.ToString().Should().Be("required");
+        }
+    }
+}
---
+++ tests/Markupolation.Tests/ElementsTests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Markupolation.Tests
+{
+    public class ElementsTests
+    {
+        [Test]
+        public void NormalElement()
+        {
+            var result = a(href("https://html.spec.whatwg.org/multipage/"), "Read the HTML Living Standard");
+            result.ToString().Should().Be("<a href=\"https://html.spec.whatwg.org/multipage/\">Read the HTML Living Standard</a>");
+
+            result = a(href("https://html.spec.whatwg.org/multipage/"), $"Read the {b($"HTML {i("Living")}")} Standard");
+            result.ToString().Should().Be("<a href=\"https://html.spec.whatwg.org/multipage/\">Read the <b>HTML <i>Living</i></b> Standard</a>");
+        }
+
+        [Test]
+        public void VoidElement()
+        {
+            var result = img(id("main-logo"), crossorigin(null), src("https://resources.whatwg.org/logo.svg"));
+            result.ToString().Should().Be("<img id=\"main-logo\" crossorigin src=\"https://resources.whatwg.org/logo.svg\" />");
+
+            result = img(id("main-logo"), crossorigin(null), src("https://resources.whatwg.org/logo.svg"), "Child");
+            result.ToString().Should().Be("<img id=\"main-logo\" crossorigin src=\"https://resources.whatwg.org/logo.svg\" />");
+        }
+    }
+}
