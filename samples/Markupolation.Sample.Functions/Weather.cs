5afa7dc9daa464828b2647e92707ca217e33ccf0
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-10-26 20:05:36 +0200

    functions

---
+++ samples/Markupolation.Sample.Functions/Weather.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Markupolation.Sample.Functions
+{
+    public static class Weather
+    {
+        [FunctionName("Weather")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            var forecasts = new[]
+            {
+                new { Date = DateTime.Parse("2018-05-06"), TemperatureC = 1, Summary = "Freezing" },
+                new { Date = DateTime.Parse("2018-05-07"), TemperatureC = 14, Summary = "Bracing" },
+                new { Date = DateTime.Parse("2018-05-08"), TemperatureC = -13, Summary = "Freezing" },
+                new { Date = DateTime.Parse("2018-05-09"), TemperatureC = -16, Summary = "Balmy" },
+                new { Date = DateTime.Parse("2018-05-10"), TemperatureC = -2, Summary = "Chilly" }
+            };
+
+            return new ContentResult
+            {
+                Content =
+                    $@"<table class=""table"">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Temp. (C)</th>
+                                <th>Temp. (F)</th>
+                                <th>Summary</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {forecasts.Each(x => tr(
+                                td(x.Date.ToShortDateString()),
+                                td(x.TemperatureC.ToString()),
+                                td(TemperatureF(x).ToString()),
+                                td(x.Summary)
+                            ))}
+                        </tbody>
+                    </table>",
+                ContentType = "text/html"
+            };
+        }
+
+        public static int TemperatureF(dynamic forecast) => 32 + (int)(forecast.TemperatureC / 0.5556);
+    }
+}
