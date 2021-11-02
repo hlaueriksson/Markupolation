fb67573cea16e760d63e1ac23cedada09526e203
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-11-02 21:04:16 +0100

    examples

---
+++ samples/Markupolation.Sample.Examples/Program.cs
@@ -0,0 +1,40 @@
+using Markupolation;
+using e = Markupolation.Elements;
+using static Markupolation.Elements;
+using a = Markupolation.Attributes;
+using static Markupolation.Attributes;
+
+var result = $@"{DOCTYPE() +
+html(lang("en"),
+    head(
+        meta(charset("utf-8")),
+        e.title("Markupolation.Sample.Api"),
+        meta(name("description"), content("Sample of how to use Markupolation")),
+        meta(name("viewport"), content("width=device-width, initial-scale=1"))
+    ),
+    body(
+        h1("Hello, World!"),
+        p("This is ", mark(a.title("Markup with string interpolation"), "Markupolation"), " in action.")
+    )
+)}";
+Console.WriteLine(result);
+
+string foo = "foo";
+string bar = null;
+result = $@"{DOCTYPE() +
+html(
+    body(
+        (foo != null ? div(foo) : string.Empty),
+        (bar != null ? div(bar) : string.Empty)
+    )
+)}";
+Console.WriteLine(result);
+
+var numbers = Enumerable.Range(0, 10).ToArray();
+result = $@"{DOCTYPE() +
+html(
+    body(
+        numbers.Each(x => div(class_(x % 2 == 0 ? "even" : "odd"), x.ToString()))
+    )
+)}";
+Console.WriteLine(result);
