65243eabcf017d6baf1ae1ad37b3997d4f6f7045
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-11-13 23:29:35 +0100

    examples

--- samples/Markupolation.Sample.Examples/Program.cs
+++ samples/Markupolation.Sample.Examples/Program.cs
@@ -1,8 +1,8 @@
 using Markupolation;
-using e = Markupolation.Elements;
 using static Markupolation.Elements;
-using a = Markupolation.Attributes;
 using static Markupolation.Attributes;
+using e = Markupolation.Elements;
+using a = Markupolation.Attributes;
 
 var result = $"{DOCTYPE() + html(head(e.title("Markupolation")), body(h1("Hello, World!")))}";
 Console.WriteLine(result);
@@ -15,7 +15,7 @@ result = $@"{DOCTYPE() +
 html(lang("en"),
     head(
         meta(charset("utf-8")),
-        e.title("Markupolation.Sample.Api"),
+        e.title("Markupolation"),
         meta(name("description"), content("Sample of how to use Markupolation")),
         meta(name("viewport"), content("width=device-width, initial-scale=1"))
     ),
@@ -37,11 +37,26 @@ html(
 )}";
 Console.WriteLine(result);
 
-var numbers = Enumerable.Range(0, 10);
+Func<int, bool> fizz = (int i) => i % 3 == 0;
+Func<int, bool> buzz = (int i) => i % 5 == 0;
+var numbers = Enumerable.Range(1, 15);
 result = $@"{DOCTYPE() +
-html(
+html(lang("en"),
+    head(
+        meta(charset("utf-8")),
+        e.title("Markupolation.Extensions"),
+        meta(name("description"), content("Sample of how to use Markupolation.Extensions")),
+        meta(name("viewport"), content("width=device-width, initial-scale=1"))
+    ),
     body(
-        numbers.Each(x => div(class_(x % 2 == 0 ? "even" : "odd"), x.ToString()))
+        ul(
+            numbers.Each(x => li(
+                x.IfMatch(i => fizz(i) && buzz(i), i => strong("FizzBuzz")),
+                x.IfMatch(i => fizz(i) && !buzz(i), i => em("Fizz")),
+                x.IfMatch(i => !fizz(i) && buzz(i), i => em("Buzz")),
+                x.IfMatch(i => !fizz(i) && !buzz(i), i => i.ToString())
+            ))
+        )
     )
 )}";
 Console.WriteLine(result);
