1e969ed48f0cc422d53f68f67becde5fbb216389
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2024-04-24 21:21:20 +0200

    :arrow_up:

--- samples/Markupolation.Sample.Aspire.AppHost/Program.cs
+++ samples/Markupolation.Sample.Aspire.AppHost/Program.cs
@@ -6,13 +6,15 @@ builder.AddProject<Projects.Markupolation_Sample_ApiGateway>("apigateway")
     .WithReference(api);
 
 builder.AddProject<Projects.Markupolation_Sample_Htmx>("htmx")
+    .WithExternalHttpEndpoints()
     .WithReference(api);
 
 //var functions = builder.AddProject<Projects.Markupolation_Sample_Functions>("functions");
 builder.AddAzureFunction<Projects.Markupolation_Sample_Functions>(
     "functions", port: 7074, debugPort: 5944);
 
-builder.AddProject<Projects.Markupolation_Sample_Blazor>("blazor");
+builder.AddProject<Projects.Markupolation_Sample_Blazor>("blazor")
+    .WithExternalHttpEndpoints();
     //.WithReference(functions);
 
 builder.Build().Run();
